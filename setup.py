a88ba841ea07ab213bef30299151fc9842748c6f
Máximo Cuadros Ortiz <mcuadros@gmail.com>
2014-08-24 22:00:12 +0200

    setup tools

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='python-orm-example',
+    license='MIT',
+    version='0.0.1',
+    install_requires=[
+        'knot >= 0.3.0',
+        'SQLAlchemy >= 0.9.7'
+    ],
+)
