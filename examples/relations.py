923cca1ed5b29461473220ee39f7f17144b4993c
Máximo Cuadros Ortiz <mcuadros@gmail.com>
2014-08-26 08:50:43 +0200

    example splitted and moved to examples folder

--- examples/relations.py
+++ examples/relations.py
@@ -1,32 +1,6 @@
 from domain.container import container
 
-# examples
 user_repository = container('models_user_repository')
-
-## Plain case
-# insert
-user = user_repository.new(email='maximo@tyba.com')
-user.first_name = 'Maximo'
-user.password = '123456'
-user_repository.insert(user)
-print 'User created with id %d' % user.id
-
-
-# find
-query = user_repository.create_query().\
-    find_by_email('maximo@tyba.com').\
-    find_active()
-
-found = user_repository.find(query).one()
-print 'User %s was created at %s' % (found.email, found.created_at)
-
-# delete
-user_repository.delete(found)
-found = user_repository.find(query).all()
-if len(found) == 0:
-    print 'Deleted success!'
-
-## Relational case
 company_repository = container('models_company_repository')
 position_repository = container('models_position_repository')
 
