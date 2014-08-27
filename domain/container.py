231a774576cd00e4c32ace1d9318541e8b2be35f
Máximo Cuadros Ortiz <mcuadros@gmail.com>
2014-08-27 08:17:31 +0200

    import full package

--- domain/container.py
+++ domain/container.py
@@ -1,20 +1,20 @@
-from knot import service
+import knot
 
 
 def apply_to_container(container):
-    @service(container)
+    @knot.service(container)
     def sqlalchemy_engine(container):
         from sqlalchemy import create_engine
 
         return create_engine(container['db.fqn'], echo=container['db.echo'])
 
-    @service(container)
+    @knot.service(container)
     def sqlalchemy_metadata(container):
         import domain.services.sql.mapper as sql
 
         return sql.Mapper().build_metadata()
 
-    @service(container)
+    @knot.service(container)
     def sqlalchemy_session(container):
         from sqlalchemy.orm import sessionmaker
 
@@ -23,19 +23,19 @@ def apply_to_container(container):
 
         return sessionmaker(bind=container('sqlalchemy_engine'))()
 
-    @service(container)
+    @knot.service(container)
     def models_user_repository(container):
         import domain.models as models
 
         return models.UserRepository(container('sqlalchemy_session'))
 
-    @service(container)
+    @knot.service(container)
     def models_position_repository(container):
         import domain.models as models
 
         return models.PositionRepository(container('sqlalchemy_session'))
 
-    @service(container)
+    @knot.service(container)
     def models_company_repository(container):
         import domain.models as models
 
